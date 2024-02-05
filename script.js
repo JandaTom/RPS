6ef684d6fa427741d9b3454c6063cb9a4be07172
JandaTom <122153993+JandaTom@users.noreply.github.com>
2024-02-05 16:12:15 +0100

    Implemented base functions to run the game
    Implemented basic solution to make the game functional. Used switch
    statements to evaluate each round and make the game run based on
    "on click" event/selection one of buttons.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function getComputerChoice() {
-    var options = ["rock", "paper", "scisors"];
+    var options = ["Rock", "Paper", "Scisors"];
 
     // random gets a random number number from 0 to 1 exclusive, that gets multiplied by length of options and rounded down by floor function 
     var choice = Math.floor(Math.random() * options.length)
@@ -7,14 +7,96 @@ function getComputerChoice() {
     return options[choice];
 }
 
-function getPlayerChoice() {
-    return prompt("Select one of 'rock / paper / scisors'").toLowerCase();
-}
+function evaluateRound(playerChoice, computerChoice) {
+    switch(playerChoice) {
+        case 'Rock':
+            switch(computerChoice) {
+                case 'Rock':
+                    document.querySelector('#singleresult').textContent = 'Tie!';
+                    break;
+                
+                case 'Paper':
+                    document.querySelector('#singleresult').textContent = 'You lose!';
+                    computerScore += 1;
+                    break;
+                
+                case 'Scisors':
+                    document.querySelector('#singleresult').textContent = 'You win!';
+                    playerScore += 1;
+                    break;
+            }
+            break;
 
-function playRound() {
-    computer = getComputerChoice();
-    player = getPlayerChoice();
+        case 'Paper':
+            switch(computerChoice) {
+                case 'Rock':
+                    document.querySelector('#singleresult').textContent = 'You win!';
+                    playerScore += 1;
+                    break;
+                
+                case 'Paper':
+                    document.querySelector('#singleresult').textContent = 'Tie!';
+                    break;
+                
+                case 'Scisors':
+                    document.querySelector('#singleresult').textContent = 'You lose!';
+                    computerScore += 1;
+                    break;
+            }
+            break;
 
-}
+        case 'Scisors':
+            switch(computerChoice) {
+                case 'Rock':
+                    document.querySelector('#singleresult').textContent = 'You lose!';
+                    computerScore += 1;
+                    break;
+                    
+                case 'Paper':
+                    document.querySelector('#singleresult').textContent = 'You win!';
+                    playerScore ++;
+                    break;
+                    
+                case 'Scisors':
+                    document.querySelector('#singleresult').textContent = 'Tie!';
+                    break;
+            }
+            break;
+    };
+};
+
+
+
+let playerScore = 0;
+let computerScore = 0;
+const play = document.querySelectorAll('button');
+play.forEach((button) => {
+    button.addEventListener('click', () => {
+            
+        document.querySelector('#finalresult').textContent = '';
+        let computerChoice = getComputerChoice();
+        let playerChoice = button.value
+        evaluateRound(playerChoice, computerChoice);
+
+        document.querySelector('#playerscore').textContent = `Player score is ${playerScore}`;
+        document.querySelector('#computerscore').textContent = `Computer score is ${computerScore}`;
+        document.querySelector('#choices').textContent = `You picked ${playerChoice} and computer picked ${computerChoice}`;
+        
+        if(playerScore == 5) {
+            document.querySelector('#finalresult').textContent = 'You smashed computer to pieces!';
+            playerScore = 0;
+            computerScore = 0;
+        }
+        
+        else if(computerScore == 5) {
+            document.querySelector('#finalresult').textContent ='You are so bad, try harder next time.';
+            playerScore = 0;
+            computerScore = 0;
+        
+        };
+        
+        });
+});
+
 
 
